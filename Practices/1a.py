50fa0771a0d9a082d5306a077cfe8b223759d06a
rebornloki-ops <lydieakokpe431@gmail.com>
2026-03-02 19:35:45 +0000

    s

---
+++ Practices/1a.py
@@ -0,0 +1,11 @@
+temp = int(input("What is the temperature in Celsius? "))
+
+if temp >30 :
+    print("It's a hot day")
+elif temp >20 :
+    print("It's a nice day")
+elif temp >10 :
+    print("Its a bit cold")
+
+else :
+    print("You should probably stay indoors")
