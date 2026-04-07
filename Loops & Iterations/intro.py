080af5fc86e2b8205e293bc54689ed1abd658b16
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-07 07:55:07 +0100

    Add comments to clarify usage of break and continue in number printing loop

--- Loops & Iterations/intro.py
+++ Loops & Iterations/intro.py
@@ -4,4 +4,6 @@ for num in nums:
     if num == 3:
         print('found!')
         break
-    print(num)
+    print(num) 
+    #If you want it to continue the number list then you simply have to replace break
+    #  (on line 6 )with continue
