36aa6006478b0e3e49e995a48cf196f7ea21302b
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-07 16:41:52 +0100

    ahhhhh fuck

--- Functions/executing-function.py
+++ Functions/executing-function.py
@@ -1,4 +1,5 @@
-def hello_func ():
-    return 'hello Function.'
+def hello_func (greeting):
+    return '{} Function.'.format(greeting)
 
-print(hello_func())
+#print(hello_func())
+print(hello_func('Hi'))
