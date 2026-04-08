4035b21658fda08d284d3bb6bdda4f0da15c4326
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-08 16:08:41 +0100

    ahem

--- Modules and Standard Lib/intro.py
+++ Modules and Standard Lib/intro.py
@@ -1,9 +1,11 @@
 
-from my_module import find_index
-
+from my_module import find_index, test
+import sys
 #import my_module as mm #The as argument is short for alias
 
 courses = ['History','Math','Physics','CompSci']
 
 index = find_index(courses,'Math')
-print(index)
+#print(index)
+#print (test)
+print (sys.path)
